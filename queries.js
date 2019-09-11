693c36699bade4443f3bcc5c64c995ce3b575e8e
Miguel Serrano <mserranocaldeiro@mserrano-ltm3.internal.salesforce.com>
2019-09-11 22:40:11 +0200

    return fixed 2

--- queries.js
+++ queries.js
@@ -15,15 +15,16 @@ console.log('Conectado a la BBDD');
 // add query functions
 
 function logEvent(org, object, replayId, operation, recordId) {
-    const result = db.one('insert into public.logtable(org, object, eventdate, replayid, operation, recordid, status) VALUES($1, $2, NOW(), $3, $4, $5, $6) RETURNING logid',
+    let result;
+    db.one('insert into public.logtable(org, object, eventdate, replayid, operation, recordid, status) VALUES($1, $2, NOW(), $3, $4, $5, $6) RETURNING logid',
             [org, object, replayId, operation, recordId, 'RECEIVED'])
         .then((data) => {
             console.log(`logEvent for ${replayId} success`);
-            return data.logid;
+            result = data.logid;
         })
         .catch((err) => {
             console.console.error(`**logEvent for ${replayId} error: ${err}`);
-            return 0;
+            result = 0;
         });
     return result;
 }
